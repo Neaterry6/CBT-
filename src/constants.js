fb8d01fceda7087a7d94450237377cf08fa6d7a3
Heisbroken 🎀🎧✍️ <189724328+Neaterry6@users.noreply.github.com>
2025-08-03 22:34:13 +0100

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -1,1 +1,16 @@
+export const API_KEYS = {
+  gemini: 'AIzaSyAkq3h7r2VN_LKJxc01jK9jslW8zzhlkuM',
+  grok: 'xai-Oep1rsF9sXto6bGJT81yjMUkSvEbQ3hz8R6kOchRc4f36lKY5Wm9mCJ91bRGHCxbFTr9NOxWzSbCk139',
+  gpt4: 'existing_gpt4_key',
+};
 
+export const ACTIVATION_PIN = '2028115';
+export const WHATSAPP_NUMBER = '+2348107993604';
+export const CONTACT_EMAIL = 'akewusholaabdulbakri101@gmail.com';
+export const CONTACT_PHONE = '+2348039896597';
+export const FACEBOOK_URL = 'https://www.facebook.com/profile.php?id=61575627958849';
+export const INSTAGRAM_URL = 'https://www.instagram.com/heisbroken6?igsh=YzljYTk1ODg3Zg==';
+export const MONGODB_URI = 'mongodb+srv://Utmecbt:isaiahilom@1@utme.7jkkmin.mongodb.net/?retryWrites=true&w=majority&appName=Utme';
+export const STRIPE_PUBLISHABLE_KEY = 'pk_test_51Rb7khFVz5lDtpGwbYCYhWAp2wIzaZUIHvd9hTqnjNGPAdE0oxhMoCepofY2FZJEtlc9RVSNmpOZzp65Ls3KzfcI00DOFXwC05';
+export const STRIPE_SECRET_KEY = 'sk_test_51Rb7khFVz5lDtpGw5FUxtKOpgJ8kWqPiHKpvNlDNKS6zhdvPRllqR45OMRrN2Ncgz2AtROsUstwCw8EWiO2KsQfn00N3sJDSMQ';
+export const CONVERSION_RATES = { time: 0.10 / 60, data: 0.01 }; // $0.10/hour, $0.01/MB
