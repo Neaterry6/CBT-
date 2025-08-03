d09c45c79dbf1bbc92fd081e06acd33f393eb8c6
Heisbroken 🎀🎧✍️ <189724328+Neaterry6@users.noreply.github.com>
2025-08-03 22:28:29 +0100

    Update stripe.js

--- src/services/stripe.js
+++ src/services/stripe.js
@@ -1,1 +1,12 @@
+import { loadStripe } from '@stripe/stripe-js';
 
+export const stripePromise = loadStripe('pk_test_51Rb7khFVz5lDtpGwbYCYhWAp2wIzaZUIHvd9hTqnjNGPAdE0oxhMoCepofY2FZJEtlc9RVSNmpOZzp65Ls3KzfcI00DOFXwC05');
+
+export const createCheckoutSession = async (amount) => {
+  const response = await fetch('http://localhost:5000/create-checkout-session', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ amount }),
+  });
+  return response.json();
+};
