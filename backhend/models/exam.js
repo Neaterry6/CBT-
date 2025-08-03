05b49264528e57949957c746ce32f07d2652f33a
Heisbroken 🎀🎧✍️ <189724328+Neaterry6@users.noreply.github.com>
2025-08-03 22:38:38 +0100

    Update exam.js

--- backhend/models/exam.js
+++ backhend/models/exam.js
@@ -1,1 +1,11 @@
+import mongoose from 'mongoose';
 
+const examSchema = new mongoose.Schema({
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  subject: String,
+  year: String,
+  score: Number,
+  date: { type: Date, default: Date.now },
+});
+
+export const Exam = mongoose.model('Exam', examSchema);
