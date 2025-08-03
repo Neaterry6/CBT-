87c1eccbd69c156c3e5a0c5ac24c173c79bb1243
Heisbroken 🎀🎧✍️ <189724328+Neaterry6@users.noreply.github.com>
2025-08-03 22:20:28 +0100

    Update Help.jsx

--- src/components/Help.jsx
+++ src/components/Help.jsx
@@ -1,1 +1,16 @@
+import React from 'react';
 
+function Help() {
+  return (
+    <div>
+      <p>Contact us at:</p>
+      <ul className="list-disc list-inside text-gray-300">
+        <li>Email: <a href="mailto:akewusholaabdulbakri101@gmail.com" className="text-blue-400 hover:underline">akewusholaabdulbakri101@gmail.com</a></li>
+        <li>Phone: <a href="tel:+2348039896597" className="text-blue-400 hover:underline">+2348039896597</a></li>
+        <li>WhatsApp: <a href="https://wa.me/2348107993604" target="_blank" className="text-blue-400 hover:underline">+2348107993604</a></li>
+      </ul>
+    </div>
+  );
+}
+
+export default Help;
