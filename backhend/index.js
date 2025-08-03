b18d80251f6e64cd5f6b3e4b786f8968d96403d5
Heisbroken 🎀🎧✍️ <189724328+Neaterry6@users.noreply.github.com>
2025-08-03 22:34:49 +0100

    Update index.js

--- backhend/index.js
+++ backhend/index.js
@@ -1,1 +1,19 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import authRoutes from './routes/auth.js';
+import paymentRoutes from './routes/payment.js';
+import dataRoutes from './routes/data.js';
+import { connectDB } from './db.js';
+import dotenv from 'dotenv';
 
+dotenv.config();
+const app = express();
+app.use(express.json());
+
+connectDB();
+
+app.use('/auth', authRoutes);
+app.use('/payment', paymentRoutes);
+app.use('/data', dataRoutes);
+
+app.listen(5000, () => console.log('Server running on port 5000'));
