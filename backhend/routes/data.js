2e531212e79ef713c49b96d2867f08a73e92b574
Heisbroken 🎀🎧✍️ <189724328+Neaterry6@users.noreply.github.com>
2025-08-03 22:37:08 +0100

    Update data.js

--- backhend/routes/data.js
+++ backhend/routes/data.js
@@ -1,1 +1,11 @@
+import express from 'express';
+import { User } from '../models/user.js';
 
+const router = express.Router();
+
+router.get('/users/:id', async (req, res) => {
+  const user = await User.findById(req.params.id);
+  res.json(user);
+});
+
+export default router;
