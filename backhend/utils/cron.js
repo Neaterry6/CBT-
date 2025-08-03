e5df86d2af21ffe741a75ee4f0768d497798f43f
Heisbroken 🎀🎧✍️ <189724328+Neaterry6@users.noreply.github.com>
2025-08-03 22:39:14 +0100

    Update cron.js

--- backhend/utils/cron.js
+++ backhend/utils/cron.js
@@ -1,1 +1,8 @@
+import cron from 'node-cron';
+import { User } from '../models/user.js';
+import { transferFunds } from '../routes/payment.js'; // Adjust import as needed
 
+cron.schedule('0 0 * * *', async () => {
+  const users = await User.find();
+  users.forEach(user => transferFunds({ userId: user._id }));
+});
