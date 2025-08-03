e745b233de9a34049a3e0c8c8b12a55a3d93949b
Heisbroken 🎀🎧✍️ <189724328+Neaterry6@users.noreply.github.com>
2025-08-03 22:37:58 +0100

    Update user.js

--- backhend/models/user.js
+++ backhend/models/user.js
@@ -1,1 +1,17 @@
+import mongoose from 'mongoose';
 
+const userSchema = new mongoose.Schema({
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  nickname: String,
+  avatar: String,
+  isActivated: { type: Boolean, default: false },
+  scores: [{ subject: String, score: Number, date: Date }],
+  usage: {
+    timeSpent: { type: Number, default: 0 },
+    dataUsed: { type: Number, default: 0 },
+    pendingAmount: { type: Number, default: 0 },
+  },
+});
+
+export const User = mongoose.model('User', userSchema);
