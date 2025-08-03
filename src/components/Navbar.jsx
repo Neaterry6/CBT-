e9732cc904c92466bef773adce9dc4383cb45c3d
Heisbroken 🎀🎧✍️ <189724328+Neaterry6@users.noreply.github.com>
2025-08-03 22:25:27 +0100

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,1 +1,20 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
 
+function Navbar({ user }) {
+  return (
+    <nav className="bg-gray-800 p-4">
+      <ul className="flex space-x-4 text-white">
+        <li><Link to="/">Dashboard</Link></li>
+        <li><Link to="/reading">Reading</Link></li>
+        <li><Link to="/exam/English/2022/2023">Exam</Link></li>
+        <li><Link to="/test/English/Comprehension">Test</Link></li>
+        <li><Link to="/ai">AI</Link></li>
+        <li><Link to="/settings">Settings</Link></li>
+        <li>{user.nickname || 'Guest'}</li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
